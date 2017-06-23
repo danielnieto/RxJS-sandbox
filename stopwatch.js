f168bd7e13cc643ec101244d1f4300690a32cf6b
Daniel Nieto <danielnieto89@gmail.com>
2017-06-23 14:23:59 -0500

    “stopwatch” with basic functionality, starts, stops and resets values

---
+++ stopwatch.js
@@ -0,0 +1,23 @@
+const startButton = document.querySelector("#start");
+const stopButton = document.querySelector("#stop");
+const resetButton = document.querySelector("#reset");
+
+const start$ = Rx.Observable.fromEvent(startButton, 'click');
+const stop$ = Rx.Observable.fromEvent(stopButton, 'click');
+const reset$ = Rx.Observable.fromEvent(resetButton, 'click');
+
+const interval$ = Rx.Observable.interval(100);
+
+const pausible$ = interval$.takeUntil(stop$);
+
+const initial = 0;
+const inc = acc => acc + 1;
+const reset = acc => initial;
+
+const incOrReset$ = Rx.Observable.merge(pausible$.mapTo(inc), reset$.mapTo(reset));
+
+const app$ = start$
+                .switchMapTo(incOrReset$)
+                .startWith(initial)
+                .scan((acc, currFunc) => currFunc(acc))
+                .subscribe(val => console.log(val));
