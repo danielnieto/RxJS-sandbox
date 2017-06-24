8406b891459aa6171ecf0d31ee20b7be8fdd4505
Daniel Nieto <danielnieto89@gmail.com>
2017-06-24 14:26:58 -0500

    create toTime function to parse milliseconds, seconds and minutes

--- stopwatch.js
+++ stopwatch.js
@@ -6,6 +6,14 @@ const start$ = Rx.Observable.fromEvent(startButton, 'click');
 const stop$ = Rx.Observable.fromEvent(stopButton, 'click');
 const reset$ = Rx.Observable.fromEvent(resetButton, 'click');
 
+const toTime = (time) => {
+    return {
+        milliseconds: Math.floor(time%100),
+        seconds: Math.floor((time/100) % 60),
+        minutes: Math.floor(time/6000),
+    }
+};
+
 const interval$ = Rx.Observable.interval(10);
 
 const stopOrReset$ = Rx.Observable.merge(stop$, reset$);
@@ -22,4 +30,4 @@ const app$ = start$
                 .switchMapTo(incOrReset$)
                 .startWith(initial)
                 .scan((acc, currFunc) => currFunc(acc))
-                .subscribe(val => console.log(val));
+                .subscribe(val => console.log(toTime(val)) );
